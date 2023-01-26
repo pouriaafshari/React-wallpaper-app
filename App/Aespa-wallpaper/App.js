703e8f3646da84f745946a9d9bedb000897cea43
pouriaafshari <pooriyaafshari81@gmail.com>
2023-01-26 16:21:23 +0100

    Creating app

--- App/Aespa-wallpaper/App.js
+++ App/Aespa-wallpaper/App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Image, View, Dimensions } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Image
+        style={styles.tinyLogo}
+        source={{
+          uri: 'https://pouriaafshari.github.io/React-wallpaper-app/DATA-BASE/AespaWallpapers/All/2.jpg',
+        }}
+      />
     </View>
   );
 }
@@ -13,8 +18,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  tinyLogo: {
+    width: Dimensions.get('screen').width,
+    height: Dimensions.get('screen').height,
+  }
 });
